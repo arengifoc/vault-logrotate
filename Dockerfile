4610f7825f0a689973cbccf10dfe1020c0470d72
Angel Rengifo <arengifoc@gmail.com>
2025-08-12 20:31:41 -0500

    Se agrega logs de rotacion en /var/log/logrotate/

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.18 AS builder
+FROM golang:latest AS builder
 WORKDIR /go/src/github.com/HanseMerkur/vault-logrotate
 COPY * ./
 RUN go get -d -v \
@@ -9,10 +9,12 @@ FROM alpine:3
 
 ENV CRONTAB="0 * * * *"
 
-# Same group/user ids as vault container
-RUN apk update --no-cache && apk add --no-cache logrotate && \
+RUN apk update --no-cache && apk upgrade --no-cache && apk add --no-cache logrotate && \
+    deluser --remove-home klogd && \
     addgroup -g 1000 crond && \
-    adduser -u 100 -S -g crond -D -H -h "/tmp" crond
+    adduser -u 100 -S -g crond -D -H -h "/tmp" crond && \
+    mkdir /var/log/logrotate && \
+    chown crond:crond /var/log/logrotate
 
 COPY --from=builder /go/src/github.com/HanseMerkur/vault-logrotate/vault-logrotate /usr/local/bin/vault-logrotate
 
--- crond.go
+++ crond.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"time"
@@ -9,24 +10,48 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
+func logInfo(message string) {
+	timestamp := time.Now().UTC().Format("Jan 02 15:04:05")
+	hostname, _ := os.Hostname()
+	log.Printf("%s %s %s", timestamp, hostname, message)
+}
+
+func logError(message string) {
+	timestamp := time.Now().UTC().Format("Jan 02 15:04:05")
+	hostname, _ := os.Hostname()
+	log.Printf("%s %s [ERROR] %s", timestamp, hostname, message)
+}
+
 func main() {
+	// Open log file
+	logFile, err := os.OpenFile("/var/log/logrotate/logrotate.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("Error opening log file: %v\n", err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+
+	// Set log output to file
+	log.SetOutput(logFile)
+	log.SetFlags(0) // Disable default timestamp since we handle it ourselves
+
 	crond := gocron.NewScheduler(time.UTC)
 	var crontab string
 	var ok bool
 	if crontab, ok = os.LookupEnv("CRONTAB"); !ok {
 		crontab = "0 * * * *"
 	}
-	fmt.Printf("Starting vault logrotation with \"%s\"\n", crontab)
+	logInfo(fmt.Sprintf("Starting vault logrotation with \"%s\"", crontab))
 	crond.Cron(crontab).Do(run_logrotate)
 	crond.StartBlocking()
 }
 
 func run_logrotate() {
 	cmd := exec.Command("/usr/sbin/logrotate", "--state=/tmp/logrotate.status", "/etc/logrotate.conf")
-	fmt.Printf("Starting logrotation\n")
+	logInfo("Starting logrotation")
 	if err := cmd.Run(); err != nil {
-		fmt.Printf("Finished logrotation with error: %v", err)
+		logError(fmt.Sprintf("Finished logrotation with error: %v", err))
 	} else {
-		fmt.Println("Finished logrotation")
+		logInfo("Finished logrotation")
 	}
 }
